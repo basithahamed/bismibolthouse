58dee9c858d6665c56bbd4b3a5c1426608dc96d9
basithahamed <basid.ahamed@zohocorp.com>
2025-01-28 00:33:21 +0530

    added index.html

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const heroSection = document.getElementById("hero");
+
+    const images = [
+        "./src/assets/bg1.jpg",
+        "./src/assets/bg2.jpg",
+        "./src/assets/silver-nails.jpg"
+    ];
+
+    let currentIndex = 0;
+
+    function changeBackground() {
+        heroSection.style.backgroundImage = `url('${images[currentIndex]}')`;
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // Change background every 5 seconds
+    setInterval(changeBackground, 5000);
+});
